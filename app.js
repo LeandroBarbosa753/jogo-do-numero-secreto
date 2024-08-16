4bf6a477c716afaf98c8654ce07ea9b03e55f988
Leandro Barbosa <101986070+LeandroBarbosa753@users.noreply.github.com>
2024-08-16 21:22:30 +0000

    :boom: fix: Revertendo mudanças ineficientes

--- app.js
+++ app.js
@@ -3,66 +3,64 @@ let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
-function exibirTextoNaTela(tag, texto){
-    let campo = document.querySelector(tag)
-    campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate: 1.2});
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+  responsiveVoice.speak(texto, "Brazilian Portuguese Female", { rate: 1.2 });
 }
-function exibirMensagemInicial(){
-    exibirTextoNaTela('h1','Jogo do número secreto');
-    exibirTextoNaTela('p','Tente adivinhar o número secreto entre 1 e 10')
+
+function exibirMensagemInicial() {
+  exibirTextoNaTela("h1", "Jogo do número secreto");
+  exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
 }
 
 exibirMensagemInicial();
 
-function verificarChute(){
-    let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto)
+function verificarChute() {
+  let chute = document.querySelector("input").value;
 
-    if(chute == numeroSecreto){
-        exibirTextoNaTela('h1', 'Acertou!');
-        let palavraTentativa = tentativas == 1 ? " tentativa" : " tentativas";
-        let mensagemTentativas = `Você descobriu o número secreto em ${tentativas} ${palavraTentativa}!`;  
-        exibirTextoNaTela("p", mensagemTentativas);
-        document.getElementById('reiniciar').removeAttribute('disabled');
+  if (chute == numeroSecreto) {
+    exibirTextoNaTela("h1", "Acertou!");
+    let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+    let mensagemTentativas = `Você descobriu o número secreto em ${tentativas} ${palavraTentativa}!`;
+    exibirTextoNaTela("p", mensagemTentativas);
+    document.getElementById("reiniciar").removeAttribute("disabled");
+  } else {
+    if (chute > numeroSecreto) {
+      exibirTextoNaTela("p", "O número secreto é menor");
+    } else {
+      exibirTextoNaTela("p", "O número secreto é maior");
     }
-    else{
-        if(chute > numeroSecreto){
-            exibirTextoNaTela("p", "O número secreto é menor");
-        }
-        else{
-            exibirTextoNaTela("p", "O número secreto é maior");
-        };
-        tentativas++;
-        limparCampo();
-    }
-}
-
-function gerarNumeroAleatorio(){
-    let numeroEscolhido = parseInt(Math.random() * numeroLimite +1);
-    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
-
-    if(quantidadeDeElementosNaLista == 10){
-        listaDeNumerosSorteados = [];
-    }
-    if(listaDeNumerosSorteados.includes(numeroEscolhido)){
-        return gerarNumeroAleatorio();
-    }else{
-        listaDeNumerosSorteados.push(numeroEscolhido);
-        return numeroEscolhido;
-    }
-}
-
-function limparCampo(){
-        chute = document.querySelector('input');
-        chute.value = '';
-        }
-
-function reiniciarJogo(){
-    numeroSecreto = gerarNumeroAleatorio();
+    tentativas++;
     limparCampo();
-    tentativas = 1;
-    exibirMensagemInicial();
-    document.getElementById('reiniciar').setAttribute('disabled', true);
-};
+  }
+}
 
+function gerarNumeroAleatorio() {
+  let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+  let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+  if (quantidadeDeElementosNaLista == numeroLimite) {
+    listaDeNumerosSorteados = [];
+  }
+  if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+    return gerarNumeroAleatorio();
+  } else {
+    listaDeNumerosSorteados.push(numeroEscolhido);
+    console.log(listaDeNumerosSorteados);
+    return numeroEscolhido;
+  }
+}
+
+function limparCampo() {
+  chute = document.querySelector("input");
+  chute.value = "";
+}
+
+function reiniciarJogo() {
+  numeroSecreto = gerarNumeroAleatorio();
+  limparCampo();
+  tentativas = 1;
+  exibirMensagemInicial();
+  document.getElementById("reiniciar").setAttribute("disabled", true);
+}
